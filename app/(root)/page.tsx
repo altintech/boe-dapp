09691eebdf89aaab19ccbe59b089ff4598210f23
Altin Hoxha <altinhoxha@Altins-MBP.fios-router.home>
2023-08-29 01:25:52 -0400

    Minor update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 import { Key } from "react";
 
@@ -13,8 +15,26 @@ async function Home({
 
       <section className="mt-9 flex flex-col gap-10 text-gray-300">
         <div>
-          <p>This is the Bills Of Exchange POC UI</p>
+          <p>This is the Trade Finance Tokenization DApp</p>
         </div>
+        <Link
+          href="/taxonomy.pdf"
+          target="_blank"
+          className="flex items-center gap-4"
+        >
+          <Image src="/taxonomy.png" alt="logo" width={200} height={200} />
+          <div>
+            <p className="text-heading3-bold text-light-1 max-xs:hidden">
+              Click on the image to see the Finance Taxonomy from International
+              Trade and Forfaiting Association
+            </p>
+            <p>
+              Tokenization on this platform uses this taxonomy. Each of the
+              instruments shown would be enabled over time with Smart Contracts
+              deployed on the Digital Assets platform
+            </p>
+          </div>
+        </Link>
       </section>
     </>
   );
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -3,22 +3,24 @@ import Link from "next/link";
 
 function Topbar() {
   return (
-    <nav className='topbar'>
-      <Link href='/' className='flex items-center gap-4'>
-        <Image src='/logo.png' alt='logo' width={40} height={40}/>
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Bills Of Exchange</p>
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.png" alt="logo" width={40} height={40} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">
+          Trade Finance Tokenization
+        </p>
       </Link>
 
-      <div className='flex items-center gap-1'>
-        <div className='block md:hidden'>
-              <div className='flex cursor-pointer'>
-                <Image
-                  src='/assets/logout.svg'
-                  alt='logout'
-                  width={24}
-                  height={24}
-                />
-              </div>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <div className="flex cursor-pointer">
+            <Image
+              src="/assets/logout.svg"
+              alt="logout"
+              width={24}
+              height={24}
+            />
+          </div>
         </div>
       </div>
     </nav>
